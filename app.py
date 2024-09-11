a6aace9a6185f84bfeba25a6334f1433dd0aa8a5
Aman <98183804+aman8808@users.noreply.github.com>
2024-09-12 04:00:40 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,11 @@ import torch
 from transformers import CLIPProcessor, CLIPModel
 import numpy as np
 
-# Загрузка модели и процессора CLIP
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-# Инициализация предобученной модели CLIP и процессора
 model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
 processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
-# Загрузка весов модели из файла .pth
 model_path = "model.pth"
 model.load_state_dict(torch.load(model_path, map_location=device))
 model.to(device)
@@ -20,38 +17,30 @@ model.eval()
 def check_brand(image, brand_name):
     brand_name_with_suffix = f"{brand_name}_logo"
 
-    # Подготовка изображения и текста
     inputs = processor(text=[brand_name_with_suffix], images=image, return_tensors="pt", padding=True)
     image_tensor = inputs['pixel_values'].to(device)
     text_tensor = inputs['input_ids'].to(device)
 
     with torch.no_grad():
-        # Извлечение признаков изображения и текста
         image_features = model.get_image_features(pixel_values=image_tensor)
         text_features = model.get_text_features(input_ids=text_tensor)
 
-        # Нормализация признаков
         image_features = image_features / image_features.norm(dim=-1, keepdim=True)
         text_features = text_features / text_features.norm(dim=-1, keepdim=True)
 
-        # Вычисление косинусного сходства
         similarity = torch.matmul(image_features, text_features.T).squeeze().cpu().numpy()
 
-        # Отладочные выводы
         print("Image Features Shape:", image_features.shape)
         print("Text Features Shape:", text_features.shape)
         print("Similarity Shape:", similarity.shape)
         print("Similarity Values:", similarity)
 
-        # Проверка на корректность размерности
         if similarity.ndim == 0:
             similarity = np.array([similarity])
 
-        # Определение соответствия
-        threshold = 0.2  # Попробуйте разные значения порога
+        threshold = 0.5  
         return similarity[0] > threshold if similarity.size > 0 else False
 
-# Создание интерфейса Streamlit
 st.title("Определение соответствия бренда")
 
 uploaded_file = st.file_uploader("Выберите изображение товара", type=["jpg", "jpeg", "png"])
